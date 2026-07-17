c45c9e045473b30768eb4cb9d0e363e8c7258f20
Biozid Bhuiyan Tonoy <150583477+Biozidtonoy@users.noreply.github.com>
2026-07-18 00:22:37 +0600

    configure PostgreSQL database connection (#34)
    - Configure SQLAlchemy engine
    - Create reusable SessionLocal
    - Add declarative Base
    - Implement get_db dependency
    - Load database settings using pydantic-settings
    - Configure environment variables through .env
    - Verify successful PostgreSQL connection

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,17 +1,25 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
+from contextlib import asynccontextmanager
+from fastapi import FastAPI
+from sqlalchemy import text
+
+from app.db.database import engine
+
+# @asynccontextmanager
+# async def lifespan(app: FastAPI):
+#     try:
+#         with engine.connect() as connection:
+#             connection.execute(text("SELECT 1"))
+#         print("✅ Successfully connected to PostgreSQL.")
+#     except Exception as e:
+#         print(f"❌ Failed to connect to PostgreSQL: {e}")
+#         raise
+#     yield
+
+# lifespan=lifespan
 
 app = FastAPI()
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-
 @app.get("/")
-def read_root(name: str = "Biozid Bhuiyan Tonoy"):
-    return {"full_name": name}
+def root():
+    return {"message": "easytrip-bd"}
