22ff99cdcd208dea3c66a217ae2f0c47e5c076dd
Biozid Bhuiyan Tonoy <150583477+Biozidtonoy@users.noreply.github.com>
2026-07-22 16:51:55 +0600

    feat : implement destination page (#53)

---
+++ frontend/src/components/destination/DestinationGrid.tsx
@@ -0,0 +1,28 @@
+import DestinationCard from "./DestinationCard";
+
+import type { Destination } from "../../types/destination";
+
+import "../../styles/destinationGrid.css";
+
+interface DestinationGridProps {
+  destinations: Destination[];
+}
+
+const DestinationGrid = ({
+  destinations,
+}: DestinationGridProps) => {
+  return (
+    <section className="destination-grid">
+
+      {destinations.map((destination) => (
+        <DestinationCard
+          key={destination.id}
+          destination={destination}
+        />
+      ))}
+
+    </section>
+  );
+};
+
+export default DestinationGrid;
---
+++ frontend/src/components/destination/Pagination.tsx
@@ -0,0 +1,9 @@
+const Pagination = () => {
+  return (
+    <div className="destination-pagination">
+      Pagination
+    </div>
+  );
+};
+
+export default Pagination;
